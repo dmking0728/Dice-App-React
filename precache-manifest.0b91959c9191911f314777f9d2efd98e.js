1fc8150eb797105c4ac68c66989975dd7142f213
daniel king <danielking@daniels-MacBook-Pro.local>
2019-07-05 14:26:11 -0400

    Updates

---
+++ precache-manifest.0b91959c9191911f314777f9d2efd98e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8342ee86ac1f851790ef67d1c25ea5f",
+    "url": "/Dice-App-React/index.html"
+  },
+  {
+    "revision": "3bdc4f70b0fb059be1ac",
+    "url": "/Dice-App-React/static/css/main.39029b9c.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/Dice-App-React/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "3bdc4f70b0fb059be1ac",
+    "url": "/Dice-App-React/static/js/main.2fa716f9.chunk.js"
+  },
+  {
+    "revision": "c6e2d05591042b0b64c4",
+    "url": "/Dice-App-React/static/js/runtime~main.4b20c848.js"
+  }
+]);
